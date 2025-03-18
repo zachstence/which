f981b15ca1516575d88e81b6a523ecbfa4ee642d
Zachary Stence <zachstence@gmail.com>
2025-03-17 19:54:33 -0500

    feat: move login/logout out of demo

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,7 +4,7 @@ import type { Actions, PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => {
 	if (!event.locals.user) {
-		return redirect(302, '/demo/lucia/login');
+		return redirect(302, '/login');
 	}
 	return { user: event.locals.user };
 };
@@ -17,6 +17,6 @@ export const actions: Actions = {
 		await auth.invalidateSession(event.locals.session.id);
 		auth.deleteSessionTokenCookie(event);
 
-		return redirect(302, '/demo/lucia/login');
+		return redirect(302, '/login');
 	}
 };
