78d5f4b965b2336e8109d5127f46bf75c8bb4975
Zachary Stence <zachstence@gmail.com>
2025-03-18 08:56:54 -0500

    fix: mobile responsiveness

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -7,19 +7,17 @@
 </script>
 
 <div class="flex h-screen w-screen flex-col items-center justify-center gap-4 overflow-hidden">
-	<header class="navbar shrink-0 select-none">
-		<span class="navbar-start text-xl italic">Which?</span>
+	<header class="navbar shrink-0 px-6 select-none">
+		<div class="navbar-start">
+			<span class="hidden text-xl italic md:block">Which?</span>
+		</div>
 		<div class="navbar-center flex flex-row items-center justify-center gap-4">
 			<a class="btn btn-primary btn-ghost" href="/">Vote</a>
 			<a class="btn btn-primary btn-ghost" href="/names">Names</a>
 			<a class="btn btn-primary btn-ghost" href="/results">Results</a>
 		</div>
-		<div class="navbar-end flex items-center justify-end">
-			<span class="px-4">{data.user.username}</span>
-			<div class="bg-base-content/50 mx-4 h-6 w-[1px]"></div>
-			<form class="contents" method="POST" action="?/logout" use:enhance>
-				<button type="submit" class="btn btn-ghost btn-error">Logout</button>
-			</form>
+		<div class="navbar-end">
+			<span class="hidden px-4 md:block">{data.user.username}</span>
 		</div>
 	</header>
 
@@ -27,7 +25,10 @@
 		{@render children()}
 	</main>
 
-	<footer class="flex w-full shrink-0 flex-row items-center justify-end">
+	<footer class="flex w-full shrink-0 flex-row items-center justify-between px-2 pb-2">
+		<form class="contents" method="POST" action="?/logout" use:enhance>
+			<button type="submit" class="btn btn-ghost btn-error">Logout</button>
+		</form>
 		<a href="https://github.com/zachstence/which" class="btn btn-ghost rounded-full">GitHub</a>
 	</footer>
 </div>
